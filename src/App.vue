4ad564fb2ead444cb0d096808be9f45e7a2fa149
Leonardo Ramirez <deiividramirez@gmail.com>
2021-10-11 12:44:59 -0500

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-system-bar app height="30" color="#256eef"></v-system-bar>
+
+    <!-- <v-app-bar app color="#256eef" dense dark> </v-app-bar> -->
+
+    <Header/>
+
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+        <!-- <v-footer app>Soy un footer</v-footer> -->
+        <!-- <v-bottom-navigation app color="secondary"></v-bottom-navigation> -->
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+
+  components: {
+    Header,
+  },
+};
+</script>
