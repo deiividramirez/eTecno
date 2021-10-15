9b2a58c45ca1c6bf8c915b1976e7b25f282a38e9
Leonardo Ramirez <deiividramirez@gmail.com>
2021-10-14 20:56:49 -0500

    Carlos

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -4,6 +4,8 @@
           <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition" transition="fade-transition"></v-carousel-item>
      </v-carousel>
 
+     <v-row
+
 </v-container>
 </template>
 
